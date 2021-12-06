6747c3daf5195d187c1f60f6c1039a7f2fef2902
Piotr Justyna <piotr.justyna@gmail.com>
2021-12-06 14:25:55 +0000

    Finished example code.

--- MercuryHost/Program.fs
+++ MercuryHost/Program.fs
@@ -1,13 +1,61 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.Net.Http
+open System.Threading
+open System.Xml.Serialization
+open MercuryLibrary
+open MercuryLibrary.Models
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let log (message: string) =
+    let timestamp = DateTime.Now.ToString("hh:mm:ss.fff")
+    printfn $"{timestamp}: {message}"
+
+let getWhoisResponse (apiUrlFormat: string) (domain: string) =
+    InputValidation.whoisInputValidation apiUrlFormat domain
+
+    let apiUrl = String.Format(apiUrlFormat, domain)
+
+    let cancellationTokenSource =
+        new CancellationTokenSource(TimeSpan.FromSeconds(3.0))
+
+    let cancellationToken = cancellationTokenSource.Token
+
+    let client = new HttpClient()
+
+    async {
+        let! apiResponse =
+            client.GetAsync(apiUrl, cancellationToken)
+            |> Async.AwaitTask
+
+        if apiResponse.IsSuccessStatusCode then
+            let serializer = XmlSerializer(typeof<WhoisRecord>)
+
+            let! stream =
+                apiResponse.Content.ReadAsStreamAsync(cancellationToken)
+                |> Async.AwaitTask
+
+            let whoisRecord =
+                serializer.Deserialize(stream) :?> WhoisRecord
+
+            return Mappers.toWhoisResponse DateTime.Now domain whoisRecord
+        else
+            return Option.None
+    }
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    log "function is starting..."
+
+    let apiUrlFormat = argv.[0]
+
+    let domain = argv.[1]
+
+    let job =
+        async { return! getWhoisResponse apiUrlFormat domain }
+
+    let response = Async.RunSynchronously(job)
+
+    printfn $"{response.ToString()}"
+
+    log "function execution finished"
+
+    0
---
+++ readme.md
@@ -0,0 +1,16 @@
+# mercury-functional
+
+Functional type of code with explicitly separated pure and effectful responsibilities. In contrast to:
+
+* [mercury-legacy](https://github.com/PiotrJustyna/mercury-legacy)
+* [mercury-pure-functional](https://github.com/PiotrJustyna/mercury-pure-functional)
+
+it is written exclusively in F# (both aspects of the code: pure and effectful). It demonstrates some useful techniques of dealing with asynchronous code.
+
+## output
+
+```
+02:21:46.400: function is starting...
+Some("google.com": Some(8848.265132695347) days since domain creation, Some(818.9046697323843) days since domain last updated, Some(2472.7348673046527) until domain expires)
+02:21:47.477: function execution finished
+```
